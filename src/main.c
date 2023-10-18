2e04825755650399cf206a2b58ab4a3f2b7984ee
ethandhunt <ethandhunt@gmail.com>
2023-10-19 12:55:57 +1300

    add apples, add border collision

--- src/main.c
+++ src/main.c
@@ -2,11 +2,16 @@
 #include <gint/keyboard.h>
 #include <gint/keycodes.h>
 #include <gint/clock.h>
+#include <stdlib.h>
 
 #define CELL_SIZE 4
 #define GRID_WIDTH (128 / CELL_SIZE)
 #define GRID_HEIGHT (64 / CELL_SIZE)
 #define GRID_LENGTH (GRID_WIDTH * GRID_HEIGHT)
+
+#define DELAY_LENGTH 200
+
+#define COORDS_INDEX_START 0
 
 struct coord
 {
@@ -16,15 +21,16 @@ struct coord
 
 unsigned char grid[GRID_LENGTH];
 struct coord coords[GRID_LENGTH];
-int coords_index = 10;
+int coords_index = COORDS_INDEX_START;
 
-void draw_VRAM()
+struct coord apple_coord;
+
+void draw()
 {
 	int x;
 	int y;
-	int i;
 	dclear(C_WHITE);
-	for (i = 0; i < GRID_LENGTH; i++)
+	for (int i = 0; i < GRID_LENGTH; i++)
 	{
 		x = (i % GRID_WIDTH) * CELL_SIZE;
 		y = (i / GRID_WIDTH) * CELL_SIZE;
@@ -33,12 +39,16 @@ void draw_VRAM()
 			drect(x, y, x + CELL_SIZE - 1, y + CELL_SIZE - 1, C_BLACK);
 		}
 	}
+
+	x = apple_coord.x * CELL_SIZE;
+	y = apple_coord.y * CELL_SIZE;
+	drect_border(x, y, x + CELL_SIZE-1, y + CELL_SIZE-1, C_WHITE, 1, C_BLACK);
 }
 
 int main(void)
 {
-	unsigned char x = 0;
-	unsigned char y = 0;
+	unsigned char x = GRID_HEIGHT/2;
+	unsigned char y = GRID_WIDTH/2;
 
 	/*
 	0 - >
@@ -48,26 +58,24 @@ int main(void)
 	*/
 	unsigned char dir = 0;
 
-	int i; // for loop variable
-
 	dclear(C_WHITE);
 
 	while (1)
 	{
 		clearevents();
-		if (keydown(KEY_RIGHT))
+		if (keydown(KEY_RIGHT) && dir != 2)
 		{
 			dir = 0;
 		}
-		if (keydown(KEY_DOWN))
+		else if (keydown(KEY_DOWN) && dir != 3)
 		{
 			dir = 1;
 		}
-		if (keydown(KEY_LEFT))
+		else if (keydown(KEY_LEFT) && dir != 0)
 		{
 			dir = 2;
 		}
-		if (keydown(KEY_UP))
+		else if (keydown(KEY_UP) && dir != 1)
 		{
 			dir = 3;
 		}
@@ -89,27 +97,48 @@ int main(void)
 			y--;
 		}
 
+		if (x == apple_coord.x && y == apple_coord.y)
+		{
+			coords_index++;
+			apple_coord.x = rand() % GRID_WIDTH;
+			apple_coord.y = rand() % GRID_HEIGHT;
+		}
+
 		if (keydown(KEY_EXIT))
 		{
 			break;
 		}
 
-		x = (x) % GRID_WIDTH;
-		y = (y) % GRID_HEIGHT;
+		// x < 0 || y < 0 unnecessary because unsigned int wraps around
+		if (grid[x + y*GRID_WIDTH] ||x >= GRID_WIDTH || y >= GRID_HEIGHT)
+		{
+			dprint(1, 1, C_BLACK, "LOSE");
+			dupdate();
+			getkey();
+
+			x = GRID_WIDTH/2;
+			y = GRID_HEIGHT/2;
+			coords_index = COORDS_INDEX_START;
+			dir = 0;
+			for (int i=0; i<GRID_LENGTH; i++)
+			{
+				grid[i] = 0;
+			}
+		}
 
 		grid[x + y * GRID_WIDTH] = 1;
 		grid[coords[0].x + coords[0].y * GRID_WIDTH] = 0;
 
-		for (i = 0; i < coords_index; i++)
+		for (int i = 0; i < coords_index; i++)
 		{
 			coords[i] = coords[i + 1];
 		}
 		coords[coords_index].x = x;
 		coords[coords_index].y = y;
 
-		draw_VRAM();
+		draw();
 		dupdate();
-		sleep_ms(50);
+		sleep_ms(DELAY_LENGTH);
 	}
 	return 1;
 }
